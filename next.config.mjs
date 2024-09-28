823468ac0c26288eccfb0697846bad0c68640b8a
Jeongmin <srm0609@naver.com>
2024-09-28 22:30:57 +0900

    add: package
    emotion, marked, react dropzone

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // 프론트엔드에서 호출할 경로
+        destination: "http://localhost:8080/:path*", // 백엔드 서버 주소
+      },
+    ];
+  },
+};
 
 export default nextConfig;
